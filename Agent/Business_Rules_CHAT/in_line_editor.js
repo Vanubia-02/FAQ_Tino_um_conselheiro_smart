d49c15cd4b2ed51e70d1c824d17bc6b1be1a38ab
Vanubia-02 <vanubia.if@gmail.com>
2024-03-17 21:39:15 -0300

    refactor: organization

--- Agent/Business_Rules_CHAT/in_line_editor.js
+++ Agent/Business_Rules_CHAT/in_line_editor.js
@@ -1,20 +1,19 @@
-// See https://github.com/dialogflow/dialogflow-fulfillment-nodejs
-// for Dialogflow fulfillment library docs, samples, and to report issues
 'use strict';
 
+//BLOCO - CONEXÃO COM FIREBASE
 const functions = require('firebase-functions');
 const {WebhookClient} = require('dialogflow-fulfillment');
 const {Card, Suggestion} = require('dialogflow-fulfillment');
 
 //BLOCO - CONEXÃO COM FIREBASE
-const admin = require('firebase-admin');
 
+const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 const db = admin.firestore();
 
 //BLOCO - CONEXÃO COM FIREBASE
- 
-process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
+
+process.env.DEBUG = 'dialogflow:debug'; 
  
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
 const agent = new WebhookClient({ request, response });
@@ -26,18 +25,15 @@ console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
 function Addemail(agent) {
   const registerEmail = agent.parameters.AddEmail;
 
-  // Verificar se o e-mail já existe na coleção "emails"
   return db.collection('Emails')
     .where('email', '==', registerEmail)
     .get()
     .then(snapshot => {
       if (!snapshot.empty) {
-        // E-mail já cadastrado
         console.log(`E-mail ${registerEmail} já cadastrado.`);
         agent.add(`⚠️E-mail ${registerEmail} já cadastrado.\n\n 📌Opções:\n\n 📝| Para adicionar um e-mail, digite "adicionar e-mail"\n\n ✅| Para listar os e-mails, digite "listar e-mails"\n\n 🗑️| Para deletar um e-mail, digite "deletar e-mail" \n\🛑| Para sair digite "Sair"`);
        
       } else {
-        // Adiciona o e-mail à coleção "emails" no Firestore
             return db.collection('Emails').add({
             email: registerEmail,
           })
@@ -64,29 +60,21 @@ function Addemail(agent) {
 //LISTAR EMAILS
   function Listemails(agent){
     
-    // Realiza uma consulta para obter todos os documentos na coleção "emails"
   return db.collection('Emails')
     .get()
     .then(snapshot => {
       if (snapshot.empty) {
-        
-        // Se a coleção estiver vazia, informe que não há e-mails cadastrados
-        //console.log('Nenhum e-mail encontrado na coleção.');
         agent.add('🔍Nenhum e-mail encontrado na coleção. \n\n📌Opções:\n\n 📝| Para adicionar um e-mail, digite "adicionar e-mail"\n\n ✅| Para listar os e-mails, digite "listar e-mails"\n\n 🗑️| Para deletar um e-mail, digite "deletar e-mail" \n\n🛑| Para sair digite "Sair"');
       } else {
          const listEmails = snapshot.docs.map(doc => doc.data().email);
-
-        // Construa a tabela formatada
         const formattedTable = listEmails.map(email => `✔️ ${email} `).join('\n');
         
         console.log('Tabela de e-mails:\n', formattedTable);
-        // Adiciona a tabela de e-mails como resposta do agente
         agent.add(`📑Lista de e-mails:\n\n${formattedTable}\n\n 📌Opções:\n\n 📝| Para adicionar um e-mail, digite "adicionar e-mail"\n\n ✅| Para listar os e-mails, digite "listar e-mails"\n\n 🗑️| Para deletar um e-mail, digite "deletar e-mail" \n\n🛑| Para sair digite "Sair"`);
         
       }
     })
     .catch(error => {
-      // Trata erros, se houver algum ao acessar o Firestore
       console.error('Erro ao listar e-mails:', error);
       agent.add('⚠️Desculpe, houve um erro ao listar os e-mails. Por favor, tente novamente. \n\n 📌Opções:\n\n 📝| Para adicionar um e-mail, digite "adicionar e-mail"\n\n ✅| Para listar os e-mails, digite "listar e-mails"\n\n 🗑️| Para deletar um e-mail, digite "deletar e-mail" \n\n🛑| Para sair digite "Sair"');
     });
@@ -94,6 +82,7 @@ function Addemail(agent) {
 
 //LISTAR EMAILS
   
+
 //DELETAR EMAIL
 function Deleteemail(agent) {
   const emailDelete = agent.parameters.DeleteEmail;
@@ -103,14 +92,11 @@ function Deleteemail(agent) {
     return agent.add('🚫Você não pode excluir o e-mail "clae@guanambi.ifbaiano.edu.br".\n Este e-mail é majoritário. \n\n 📌Opções:\n\n 📝| Para adicionar um e-mail, digite "adicionar e-mail"\n\n ✅| Para listar os e-mails, digite "listar e-mails"\n\n 🗑️| Para deletar um e-mail, digite "deletar e-mail" \n\n🛑| Para sair digite "Sair"');
   }
 
-  // Exclui o e-mail da coleção "emails" no Firestore
   return db.collection('Emails')
     .where('email', '==', emailDelete)
     .get()
     .then(snapshot => {
       if (!snapshot.empty) {
-        // Pode haver vários documentos com o mesmo e-mail, 
-        // mas estamos excluindo apenas o primeiro encontrado.
         const docId = snapshot.docs[0].id;
         return db.collection('Emails').doc(docId).delete();
       } else {
@@ -121,31 +107,10 @@ function Deleteemail(agent) {
       agent.add(`🗑️E-mail "${emailDelete}" excluído com sucesso. \n\n 📌Opções:\n\n 📝| Para adicionar um e-mail, digite "adicionar e-mail"\n\n ✅| Para listar os e-mails, digite "listar e-mails"\n\n 🗑️| Para deletar um e-mail, digite "deletar e-mail" \n\n🛑| Para sair digite "Sair"`);
     })
     .catch(error => {
-      //console.error('Erro ao excluir e-mail:', error);
       agent.add('⚠️Desculpe, houve um erro ao excluir o e-mail. Por favor, tente novamente. \n\n 📌Opções:\n\n 📝| Para adicionar um e-mail, digite "adicionar e-mail"\n\n ✅| Para listar os e-mails, digite "listar e-mails"\n\n 🗑️| Para deletar um e-mail, digite "deletar e-mail" \n\n🛑| Para sair digite "Sair"');
     });
 }
 //DELETAR EMAIL
- 
-
-  // // Uncomment and edit to make your own intent handler
-  // // uncomment `intentMap.set('your intent name here', yourFunctionHandler);`
-  // // below to get this function to be run when a Dialogflow intent is matched
-  // function yourFunctionHandler(agent) {
-  //   agent.add(`This message is from Dialogflow's Cloud Functions for Firebase editor!`);
-  //   agent.add(new Card({
-  //       title: `Title: this is a card title`,
-  //       imageUrl: 'https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png',
-  //       text: `This is the body text of a card.  You can even use line\n  breaks and emoji! 💁`,
-  //       buttonText: 'This is a button',
-  //       buttonUrl: 'https://assistant.google.com/'
-  //     })
-  //   );
-  //   agent.add(new Suggestion(`Quick Reply`));
-  //   agent.add(new Suggestion(`Suggestion`));
-  //   agent.setContext({ name: 'weather', lifespan: 2, parameters: { city: 'Rome' }});
-  // }
-  
   
   
 //BLOCO DE DADOS PESSOAIS DOS ALUNOS
@@ -204,7 +169,6 @@ function Deleteemail(agent) {
     return db.collection('Users').add(dataStudant)
         .then((user) => {
             const idUser = user.id;
-            //console.log(`Documento salvo com sucesso. ID do documento: ${idUser}`);
       		agent.add(`🤖 ${nameString}, escolha uma das opções sobre atendimento psicológico do Campus Guanambi: \n\n01➡️ Atendimento pelo NAPNE \n02➡️ Atendimento pelo NAPSI \n03➡️ Atendimento pelo PRO-SAÚDE \n04➡️ Atendimento pelo PAE `);
       		agent.context.set({ name: 'context', lifespan: 50, parameters: { idUser: idUser } });
             return idUser;
@@ -221,15 +185,13 @@ function Deleteemail(agent) {
   const documentUser =  db.collection('Users').doc(id);
   let currentDate = {};
 
-  // Passo 1: Obter dados atuais do documento
   return documentUser
     .get()
     .then((doc) => {
       if (doc.exists) {
-        // Se o documento existe, atualize os dados
         currentDate = doc.data();
         if(questionVersion){
-			//se forem as questões do critério A
+          
           currentDate.Criterion.TAG[criterion][questionVersion][answerKey] = answerValue;
         }
         else{
@@ -237,11 +199,9 @@ function Deleteemail(agent) {
           currentDate.Criterion.TAG[criterion][answerKey] = answerValue;
         }
 
-        // Passo 2: Atualizar o documento com os novos dados
         return documentUser.update(currentDate);
       } else {
         console.log('Documento não encontrado!');
-        // Se o documento não existe, não há necessidade de atualizar
         return Promise.reject(new Error('Documento não encontrado!'));
       }
     })
@@ -253,6 +213,8 @@ function Deleteemail(agent) {
     });
 }
   
+
+//--------------------------------- CRITÉRIOS --------------------------------
  
 //critério A1 versão 1
   function QuestionTAG1av1(agent, idUser) {
@@ -262,7 +224,6 @@ function Deleteemail(agent) {
       const id = context.parameters.idUser;
      
      return saveInBd(id, 'CriterioA', answertag1AV1, 'ResponseA1V1',  'A1');
-
 }    
 
 //critério F1 versão 2 - resposta egativa
@@ -271,7 +232,6 @@ function Deleteemail(agent) {
     
       const context = agent.context.get('context');
       const id = context.parameters.idUser;
-    
       return saveInBd(id, 'CriterioF', 'Não', 'ResponseFV2');
   }
 
@@ -292,7 +252,6 @@ function Deleteemail(agent) {
     
       const context = agent.context.get('context');
       const id = context.parameters.idUser;
-    
       return saveInBd(id, 'CriterioA', answertag2AV1, 'ResponseA2V1',  'A2');
   }
 
@@ -302,7 +261,6 @@ function Deleteemail(agent) {
     
       const context = agent.context.get('context');
       const id = context.parameters.idUser;
-    
       return saveInBd(id, 'CriterioB', answertag1BV1, 'ResponseBV1');
   }
  
@@ -312,7 +270,6 @@ function Deleteemail(agent) {
     
       const context = agent.context.get('context');
       const id = context.parameters.idUser;
-    
       return saveInBd(id, 'CriterioA', answertag1AV3, 'ResponseA1V3',  'A1');
   }
 
@@ -321,8 +278,7 @@ function Deleteemail(agent) {
       const answertag1DV1 = agent.parameters.Question1DV1;
     
       const context = agent.context.get('context');
-      const id = context.parameters.idUser;
-    
+      const id = context.parameters.idUser;   
       return saveInBd(id, 'CriterioD', answertag1DV1, 'ResponseDV1');    
   }
 
@@ -331,8 +287,7 @@ function Deleteemail(agent) {
       const answertag1DV2 = agent.parameters.Question1DV2;
     
       const context = agent.context.get('context');
-      const id = context.parameters.idUser;
-    
+      const id = context.parameters.idUser;    
       return saveInBd(id, 'CriterioD', answertag1DV2, 'ResponseDV2');
   } 
 
@@ -341,8 +296,7 @@ function Deleteemail(agent) {
       const answertag1CV2 = agent.parameters.Question1CV2;
     
       const context = agent.context.get('context');
-      const id = context.parameters.idUser;
-    
+      const id = context.parameters.idUser;   
       return saveInBd(id, 'CriterioC', answertag1CV2, 'ResponseCV2');
   }
 
@@ -352,7 +306,6 @@ function Deleteemail(agent) {
     
       const context = agent.context.get('context');
       const id = context.parameters.idUser;
-    
       return saveInBd(id, 'CriterioB', answertag1BV2, 'ResponseBV2');
   }
 
@@ -362,7 +315,6 @@ function Deleteemail(agent) {
     
       const context = agent.context.get('context');
       const id = context.parameters.idUser;
-    
       return saveInBd(id, 'CriterioE', answertag1EV1, 'ResponseEV1');
   }
 
@@ -372,8 +324,7 @@ function Deleteemail(agent) {
     
       const context = agent.context.get('context');
       const id = context.parameters.idUser;
-	 
-     return saveInBd(id, 'CriterioA', answertag1AV2, 'ResponseA1V2', 'A1');
+      return saveInBd(id, 'CriterioA', answertag1AV2, 'ResponseA1V2', 'A1');
   }
 
 //critério A2 versão 2
@@ -382,7 +333,6 @@ function Deleteemail(agent) {
     
       const context = agent.context.get('context');
       const id = context.parameters.idUser;
-    
       return saveInBd(id, 'CriterioA', answertag2AV2, 'ResponseA2V2',  'A2');
   }
 
@@ -392,7 +342,6 @@ function Deleteemail(agent) {
     
       const context = agent.context.get('context');
       const id = context.parameters.idUser;
-    
       return saveInBd(id, 'CriterioE', answertag1EV2, 'ResponseEV2');
   }
 
@@ -402,7 +351,6 @@ function Deleteemail(agent) {
     
       const context = agent.context.get('context');
       const id = context.parameters.idUser;
-    
       return saveInBd(id, 'CriterioC', answertag1CV1, 'ResponseCV1');
   }
 
@@ -412,7 +360,6 @@ function Deleteemail(agent) {
     
       const context = agent.context.get('context');
       const id = context.parameters.idUser;
-    
       return saveInBd(id, 'CriterioF', answertag1FV1yes, 'ResponseFV1');
   }
 
@@ -421,11 +368,11 @@ function Deleteemail(agent) {
     
       const context = agent.context.get('context');
       const id = context.parameters.idUser;
-    
       return saveInBd(id, 'CriterioF', 'Não', 'ResponseFV1');
   }
 
-//BLOCO DE QUESTÕES TAG  
+//--------------------------------- CRITÉRIOS --------------------------------
+ 
  
 
 //Mapeamento das Intenções
@@ -436,7 +383,7 @@ function Deleteemail(agent) {
  intentMap.set('login.add.email', Addemail);
  intentMap.set('login.list.emails',Listemails);
  intentMap.set('login.delete.emails',Deleteemail);
- intentMap.set('option1.option1',QuestionTAG1av1);//ok 
+ intentMap.set('option1.option1',QuestionTAG1av1);
  intentMap.set('option1.option2 - no',Question1fv2_no);
  intentMap.set('option1.option2 - yes', Question1fv2_yes);
  intentMap.set('option1.option3',QuestionTAG2av1);
--- README.md
+++
@@ -1,2 +0,0 @@
-# FAQ_Tino_um_conselheiro_smart
-Projeto de Conclusão de Curso. Desenvolvimento de um chatbot que identifica sinais de ansiedade, nomeado de FAQ Tino: um conselheiro smart. 
